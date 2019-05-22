44900ddd175f84a5277ce7c99ec0b681489d4e0f
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-05-22 09:22:59 -0400

    Start of a basic menu

--- 01-flxsubstate/source/MainState.hx
+++ 01-flxsubstate/source/MainState.hx
@@ -1,16 +1,30 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.FlxG;
+import flixel.ui.FlxButton;
+
+// there is no haxeui code in this state
 
 class MainState extends FlxState
 {
     override public function create():Void
     {
         super.create();
+
+        // button/desc for flxui example
+        this.add(new FlxText(150, 50, 0, "Example of intended behavior in Flixel UI (not appearance, not haxeui)"));
+        this.add(new FlxButton(50, 50, "Example", this.on_example));
     }
 
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
+    }
+
+    public function on_example():Void
+    {
+        FlxG.log.add("Pressed!");
     }
 }
