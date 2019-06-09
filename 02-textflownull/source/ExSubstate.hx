213b5d03266b7674db805755f4aa0f2c44550b21
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-06-09 01:25:34 -0400

    Copy over states from current project files

--- 02-textflownull/source/ExSubstate.hx
+++ 02-textflownull/source/ExSubstate.hx
@@ -1,4 +1,4 @@
-package;
+package net.darkglass.consume.substate;
 
 import flixel.group.FlxGroup;
 import flixel.FlxSprite;
@@ -8,13 +8,19 @@ import net.darkglass.util.flixel.FlxHaxeUiSubstate;
 import haxe.ui.Toolkit;
 import haxe.ui.macros.ComponentMacros;
 import haxe.ui.components.Label;
+import haxe.ui.containers.VBox;
 import haxe.ui.components.Button;
 import haxe.ui.events.UIEvent;
 
 import openfl.utils.Assets;
 
-class ExSubstate extends FlxHaxeUiSubstate
+import yaml.Yaml;
+import yaml.util.ObjectMap;
+
+class FAQSubstate extends FlxHaxeUiSubstate
 {
+    public static var outStr:String = "";
+
     override public function create():Void
     {
         // parent
@@ -23,10 +29,15 @@ class ExSubstate extends FlxHaxeUiSubstate
         // instance vars
         var uiGroup = new FlxGroup();
 
+        // fake bg
+        var bg_faked:FlxSprite = new FlxSprite(23, 23, "assets/images/gui/classic/bg/faq_faked.png");
+        this.add(bg_faked);
+
         // ui toolkit
         Toolkit.screen.options = { container : uiGroup };
         this.add(uiGroup);
-        var _ui = ComponentMacros.buildComponent("assets/substate.xml");
+        
+        var _ui = ComponentMacros.buildComponent("assets/ui/substate/faq.xml");
         uiGroup.add(_ui);
 
         var cout:Label = _ui.findComponent("cout", Label);
@@ -35,8 +46,49 @@ class ExSubstate extends FlxHaxeUiSubstate
         var but:Button = _ui.findComponent("close", Button);
         but.onClick = this.onClick_back;
 
-        // load text
-        cout.text = Assets.getText("assets/lorem.txt");
+
+        // load text from YAML
+        this.loadText(cout);
+    }
+
+    private function loadText(cout:Label):Void
+    {
+        if ("" == outStr)
+        {
+            // eventually
+            var outTxt:String = "";
+
+            // easier this way for me
+            var divider:String = "-----------------------------------------------";
+
+            // load source
+            var txtSrc:String = Assets.getText("assets/data/en-us/faq.yaml");
+            var txtDat:Array<ObjectMap<String, Dynamic>> = Yaml.parse(txtSrc);
+
+            // we iterate over it
+            for (entry in txtDat)
+            {
+                // real entries have an id greater than zero
+                if (entry.get("id") >= 0)
+                {
+                    // only a divider if it's not the first
+                    if (entry.get("id") != 0)
+                    {
+                        outTxt = outTxt + divider + "\n\n";
+                    }
+                    
+                    // question and answer
+                    outTxt = outTxt + "Q: " + entry.get("q") + "\n\n";
+                    outTxt = outTxt + "A: " + entry.get("a") + "\n\n";
+                }
+            }
+
+            // stash to use later
+            outStr = outTxt;
+        }
+
+        // set it, finally
+        cout.text = outStr;
     }
 
     private function onClick_back(ignored:UIEvent):Void
--- 02-textflownull/source/MainState.hx
+++ 02-textflownull/source/MainState.hx
@@ -1,30 +1,63 @@
 package;
 
-import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.ui.FlxButton;
+import openfl.Assets;
+import flixel.addons.ui.FlxUIState;
 
-import ExSubstate;
+import flixel.addons.ui.FlxUISprite;
+import flixel.addons.ui.FlxUIText;
+import flixel.addons.ui.FlxUIButton;
+import flixel.FlxG;
+import flixel.math.FlxRandom;
+import flixel.text.FlxText.FlxTextFormat;
 
-// there is no haxeui code in this state
-// I'm aware that I didn't init haxeui here. Half the point.
-class MainState extends FlxState
+import net.darkglass.consume.PlayState;
+import net.darkglass.consume.Registry;
+
+import net.darkglass.consume.substate.CreditsSubstate;
+import net.darkglass.consume.substate.FAQSubstate;
+import net.darkglass.consume.substate.OptionSubstate;
+import net.darkglass.consume.substate.PreWarnSubstate;
+
+import yaml.Yaml;
+
+// new version pieces
+import net.darkglass.util.flixel.FlxHaxeUiState;
+import flixel.group.FlxGroup;
+import flixel.FlxSprite;
+import haxe.ui.Toolkit;
+import haxe.ui.macros.ComponentMacros;
+import haxe.ui.components.Button;
+import haxe.ui.events.UIEvent;
+
+class TitleState extends FlxHaxeUiState
 {
+    public var uiGroup:FlxGroup;
+
     override public function create():Void
     {
+        // parent
         super.create();
 
-        this.add(new FlxText(150, 50, 0, "Click to open substate"));
-        this.add(new FlxButton(50, 50, "Button", this.on_button));
+        // set ui stuffs
+        this.uiGroup = new FlxGroup();
+
+        var bg:FlxSprite = new FlxSprite(0, 0, "assets/images/gui/classic/bg/title_faked.png");
+        this.add(bg);
+
+        // init ui loader
+        Toolkit.screen.options = { container : this.uiGroup };
+        this.add(this.uiGroup);
+        var _ui = ComponentMacros.buildComponent("assets/ui/titlestate.xml");
+        uiGroup.add(_ui);
+
+        // wire up the buttons
+        var faq =  _ui.findComponent("faq", Button);
+        faq.onClick = this.onClick_faq;
     }
 
-    override public function update(elapsed:Float):Void
+    public function onClick_faq(ignored:UIEvent):Void
     {
-        super.update(elapsed);
-    }
-
-    public function on_button():Void
-    {
-        openSubState(new ExSubstate(0x80000000, this));
+        var faqSubstate:FAQSubstate = new FAQSubstate(0x80000000, this.uiGroup);
+        openSubState(faqSubstate);
     }
 }
